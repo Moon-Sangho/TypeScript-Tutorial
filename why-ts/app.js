bcb5b5dc608424a713911d4779129078be463876
Moon-Sangho <skym887@gmail.com>
2020-12-29 15:30:56 +0900

    자바스크립트에 타입스크립트 적용하기

--- why-ts/app.js
+++ why-ts/app.js
@@ -1,21 +1,47 @@
 // api url
-var url = 'https://jsonplaceholder.typicode.com/users/1';
+var url = "https://jsonplaceholder.typicode.com/users/1";
 
 // dom
-var username = document.querySelector('#username');
-var email = document.querySelector('#email');
-var address = document.querySelector('#address');
+var username = document.querySelector("#username");
+var email = document.querySelector("#email");
+var address = document.querySelector("#address");
 
 // user data
 var user = {};
 
+// 서버에서 받아오는 데이터들의 타입을 미리 지정해 줌으로써 컴파일 단계에서 사전에 에러 방지 가능
+/**
+ * @typedef {object} Address
+ * @property {string} street
+ * @property {string} city
+ */
+
+/**
+ * @typedef {object} User
+ * @property {string} name
+ * @property {string} email
+ * @property {Address} address
+ */
+
+/**
+ *  @returns {Promise<User>}
+ */
+function fetchUser() {
+  return axios.get(url);
+}
+
 function startApp() {
-  axios
-    .get(url)
+  fetchUser()
     .then(function (response) {
-      console.log(response);
+      // 서버에서 받아오는 데이터 확인
+      console.log(response.data);
       user = response.data;
       // TODO: 이름, 이메일, 주소 표시하기
+      // 데이터가 user 변수에 잘 저장되었는지 확인
+      console.log(user);
+      username.innerText = user.name;
+      email.innerText = user.email;
+      address.innerText = user.address.city;
     })
     .catch(function (error) {
       console.log(error);
