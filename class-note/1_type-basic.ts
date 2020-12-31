d96b7ce59b75e486a331446e34738f48a3f508f8
Moon-Sangho <skym887@gmail.com>
2020-12-31 14:15:37 +0900

    기본 타입 - 문자열, 숫자, 배열

---
+++ class-note/1_type-basic.ts
@@ -0,0 +1,13 @@
+// JS 문자열 선언
+// var str = 'hello';
+
+// TS 문자열 선언
+let str: string = 'hello';
+
+// TS 숫자
+let num: number = 10;
+
+// TS 배열
+let arr: Array<number> = [1, 2, 3];
+let items: number[] = [1, 2, 3];
+let heroes: Array<string> = ['Capt', 'Thor', 'Hulk'];
