d65e256873900307da1b383fa73b6e02819faf04
Moon-Sangho <skym887@gmail.com>
2020-12-31 14:27:24 +0900

    기본 타입 - 튜플, 객체, 진위값

--- class-note/1_type-basic.ts
+++ class-note/1_type-basic.ts
@@ -11,3 +11,23 @@ let num: number = 10;
 let arr: Array<number> = [1, 2, 3];
 let items: number[] = [1, 2, 3];
 let heroes: Array<string> = ['Capt', 'Thor', 'Hulk'];
+
+// TS 튜플: 모든 인덱스에 타입이 정의되어 있는 배열
+let address: [string, number] = ['gangnam', 100];
+
+// TS 객체
+let obj: object = {};
+
+let person: object = {
+  name: 'capt',
+  age: 100
+};
+
+// 아래와 같이 객체 속성의 타입까지 구체적으로 지정할 수 있음
+let person2: { name: string, age: number } = {
+  name: 'capt',
+  age: 100
+}
+
+// TS 진위값
+let show: boolean = true;
