6e5b0a0bc2cd3a28dd0b730504bf53f5207111d1
Moon-Sangho <skym887@gmail.com>
2021-01-06 14:13:40 +0900

    제네릭 실전 예제 - interface, union type을 이용하여 코드에 타입 정의하기

--- example/dropdown-generic.ts
+++ example/dropdown-generic.ts
@@ -1,16 +1,27 @@
-const emails = [
+// interface와 Union Type을 이용한 방법
+interface Email {
+  value: string;
+  selected: boolean;
+}
+
+const emails: Email[] = [
   { value: 'naver.com', selected: true },
   { value: 'gmail.com', selected: false },
   { value: 'hanmail.net', selected: false },
 ];
 
-const numberOfProducts = [
+interface ProductNumber {
+  value: number;
+  selected: boolean;
+}
+
+const numberOfProducts: ProductNumber[] = [
   { value: 1, selected: true },
   { value: 2, selected: false },
   { value: 3, selected: false },
 ];
 
-function createDropdownItem(item) {
+function createDropdownItem(item: Email | ProductNumber) {
   const option = document.createElement('option');
   option.value = item.value.toString();
   option.innerText = item.value.toString();
@@ -24,3 +35,8 @@ emails.forEach(function (email) {
   const selectTag = document.querySelector('#email-dropdown');
   selectTag.appendChild(item);
 });
+
+// 아래와 같이 코드를 작성해도 에러가 발생하지 않음.
+numberOfProducts.forEach(function (product) {
+  const item = createDropdownItem(product);
+})
