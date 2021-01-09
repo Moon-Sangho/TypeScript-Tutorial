c1ad40d95ee781677301bc1e038a2b4e2d4b6837
Moon-Sangho <skym887@gmail.com>
2021-01-09 14:31:42 +0900

    타입 가드 개념 학습

---
+++ class-note/11_tpye-guard.ts
@@ -0,0 +1,35 @@
+interface Developer {
+  name: string;
+  skill: string;
+}
+
+interface Person {
+  name: string;
+  age: number;
+}
+
+function introduce(): Developer | Person {
+  return { name: 'Tony', age: 33, skill: 'Iron Making' }
+}
+
+var tony = introduce();
+// console.log(tony.skill); // name만 접근 가능
+
+if ((tony as Developer).skill) {
+  var skill = (tony as Developer).skill;
+  console.log(skill);
+} else if ((tony as Person).age) {
+  var age = (tony as Person).age;
+  console.log(age);
+}
+
+// 타입 가드 정의
+function isDeveloper(target: Developer | Person): target is Developer {
+  return (target as Developer).skill !== undefined;
+}
+
+if (isDeveloper(tony)) {
+  console.log(tony.skill);
+} else {
+  console.log(tony.age);
+}
